ade6691db556ea8b7f2541b049264b8c0976df50
Patrick Laughrea <patlaughweb@gmail.com>
2020-10-06 22:51:25 +0000

    Lower success requirements

--- alg_lv_success.py
+++ alg_lv_success.py
@@ -1,12 +1,12 @@
 # /t500/c04
 
 # configs
-NEXT_LV_PERC = 0.995
+NEXT_LV_PERC = 0.99
 NEXT_LV_WPM = 30
 NEXT_LV_WPM_INC = 1
 
-SAME_LV_PERC = 0.990
-SAME_LV_WPM = 30
+SAME_LV_PERC = 0.95
+SAME_LV_WPM = 20
 SAME_LV_WPM_INC = 1
 
 def lv_success(weighted_correct_perc, wpm, lv):
@@ -16,7 +16,7 @@ def alg_simple(correct_perc, wpm, lv):
 	req_perc = NEXT_LV_PERC
 	req_speed = NEXT_LV_WPM + lv * NEXT_LV_WPM_INC
 	if correct_perc >= req_perc and wpm >= req_speed:
-		return int(wpm / req_speed)
+		return 1
 	
 	req_perc = SAME_LV_PERC
 	req_speed = SAME_LV_WPM + lv * SAME_LV_WPM_INC
