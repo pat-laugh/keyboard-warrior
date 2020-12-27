30f3ac2c5d1bb95f1b0bde89ce16c81a4077b31c
Patrick Laughrea <patlaughweb@gmail.com>
2020-12-27 01:14:31 +0000

    T(204) Implement complex error percentage algorithm

--- alg_err_perc.py
+++ alg_err_perc.py
@@ -1,9 +1,20 @@
 # /t500/c04
 
+import difflib
+
 def err_perc(seq, in_str):
 	# THROWS: KeyboardInterrupt
 	# STATUS: DONE
-	return alg_basic(seq, in_str)
+	# alg = alg_basic
+
+	# T(204)
+	alg = alg_complex
+
+	if type(seq) is str:
+		assert(type(in_str) is str)
+		return alg(seq, in_str)
+	assert type(seq) is list and type(in_str) is list and len(seq) == len(in_str)
+	return [alg(seq[i], in_str[i]) for i in range(len(seq))]
 
 def _chars_dict(all_chars, str):
 	# count the number of each char of all_chars in str
@@ -15,7 +26,7 @@ def _chars_dict(all_chars, str):
 		dict[c] += 1
 	return dict
 
-def _alg_basic(seq, in_str):
+def alg_basic(seq, in_str):
 	# see /t500/c03/basic
 	# STATUS: DONE
 	assert type(seq) is str and type(in_str) is str
@@ -28,15 +39,7 @@ def _alg_basic(seq, in_str):
 		tt_diff += abs(chars_seq[c] - chars_in[c])
 	return min(1, tt_diff / len(seq))
 
-def alg_basic(seq, in_str):
-	# STATUS: DONE
-	if type(seq) is str:
-		return _alg_basic(seq, in_str)
-	assert type(seq) is list and type(in_str) is list and len(seq) == len(in_str)
-	err_perc_list = []
-	for i in range(len(seq)):
-		err_perc_list.append(_alg_basic(seq[i], in_str[i]))
-	return err_perc_list
-
 def alg_complex(seq, in_str):
-	assert False, 'not implemented yet'
+	# T(204)
+	sm = difflib.SequenceMatcher(None, in_str, seq)
+	return min(1, abs(1 - sm.ratio()))
